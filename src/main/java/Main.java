90bfb1c499bac260460d8757436225dbc4ca3ad5
AnanranG <jiaqiao.han@sjsu.edu>
2024-12-02 00:06:08 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import model.DataModel;
 public class Main {
     public static void main(String[] args) {
         DataModel model = new DataModel();
-        new Frame1(model);
+        Frame1 frame = new Frame1(model);
+        frame.setVisible(true);
     }
 }
-
