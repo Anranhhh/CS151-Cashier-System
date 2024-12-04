7ac5dcfa435664c6323e9c7d7a1d02ccc6c8ad92
galacticmice <github@bkyha.com>
2024-12-03 18:19:09 -0800

    Add description method for Item

--- src/main/java/domain/Item.java
+++ src/main/java/domain/Item.java
@@ -28,4 +28,8 @@ public class Item {
     public String toString() {
         return String.format("%-12s %-20s %-25s %10.2f", code, name, description, price);
     }
+
+    public String getDescription() {
+        return this.description;
+    }
 }
